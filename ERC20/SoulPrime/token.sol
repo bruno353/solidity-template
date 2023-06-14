2497196ff00a41b9ae99f6daa9004a5539728a8e
Bruno Santos <brunolsantos152@gmail.com>
2023-06-14 14:09:46 -0300

    Update token.sol

--- ERC20/SoulPrime/token.sol
+++ ERC20/SoulPrime/token.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: MIT
+
+/// @title SoulPrime
+/// @author Soulprime team
+/// @notice Este contrato é para o token SoulPrime.
+/// @dev Este contrato cria um token ERC20 e faz a mintagem inicial para um endereço específico.
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    //Passar o address do contrato de vesting - mintando 500 milhões de tokens.
+contract SoulPrime is ERC20 {
+    /// @notice Cria o token SoulPrime e faz a mintagem inicial.
+    /// @dev O total de suprimento é 500 milhões e é enviado para o endereço fornecido no parâmetro do constructor.
+    /// @param _address O endereço que receberá os tokens mintados.
     constructor(address _address) ERC20("SoulPrime", "Soul") {
-        _mint(_address, 500_000_000_000000000000000000);
+        _mint(_address, 500_000_000 ether);
     }
 }
