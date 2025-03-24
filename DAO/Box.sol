6eadfdec97b91cb06a1eb8ed735eb66a9ad13d70
Bruno Laureano <brunolsantos152@gmail.com>
2025-03-24 12:56:10 -0300

    Update Box.sol

--- DAO/Box.sol
+++ DAO/Box.sol
@@ -4,19 +4,34 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title Box
+ * @dev A simple contract for storing and retrieving a uint256 value.
+ *      Inherits from Ownable, so only the owner can update the value.
+ */
 contract Box is Ownable {
-  uint256 private value;
+    uint256 private _value;
 
-  // Emitted when the stored value changes
-  event ValuedChanged(uint256 newValue);
+    /**
+     * @dev Emitted when the stored value changes.
+     */
+    event ValueChanged(uint256 newValue);
 
-  function store(uint256 newValue) public {
-    value = newValue + 1;
-    emit ValueChanged(newValue);
-  }
+    /**
+     * @dev Stores a new value in the contract.
+     * @param newValue The new value to store.
+     *      Restricted to the contract owner.
+     */
+    function store(uint256 newValue) public onlyOwner {
+        _value = newValue;
+        emit ValueChanged(newValue);
+    }
 
-  // Reads the last stored value
-  function retrieve() public view returns (uint256) {
-    return value;
-  }
+    /**
+     * @dev Reads the last stored value.
+     * @return The last stored value.
+     */
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
 }
