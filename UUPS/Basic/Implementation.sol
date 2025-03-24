7b63c522643be50bad3f376dd58cebc3ffa13a84
Bruno Laureano <brunolsantos152@gmail.com>
2025-03-24 17:59:11 -0300

    Update Implementation.sol

--- UUPS/Basic/Implementation.sol
+++ UUPS/Basic/Implementation.sol
@@ -1,32 +1,55 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
 import "./Proxiable.sol";
 
 contract MyContract is Proxiable {
-
     address public owner;
-    uint public count;
-    bool public initalized = false;
+    uint256 public count;
+    bool public initialized;
 
-    function initialize() public {
-        require(owner == address(0), "Already initalized");
-        require(!initalized, "Already initalized");
+    /**
+     * @notice Initializes the contract, setting the deployer as owner.
+     *         Can only be called once.
+     */
+    function initialize() external {
+        require(!initialized, "Already initialized");
         owner = msg.sender;
-        initalized = true;
+        initialized = true;
     }
 
-    function increment() public {
+    /**
+     * @notice Increments the counter by 1.
+     */
+    function increment() external {
         count++;
     }
 
-    function updateCode(address newCode) onlyOwner public {
+    /**
+     * @notice Updates the implementation address for this proxy.
+     *         Only the contract owner can call this function.
+     * @param newCode The address of the new implementation contract.
+     */
+    function updateCode(address newCode) external onlyOwner {
+        require(newCode != address(0), "Invalid code address");
         updateCodeAddress(newCode);
     }
 
+    /**
+     * @notice Allows the current owner to transfer control of the contract
+     *         to a new owner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid new owner");
+        owner = newOwner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner is allowed to perform this action");
+        require(msg.sender == owner, "Only owner can call this");
         _;
     }
 }
