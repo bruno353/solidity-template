ec0963e7ee29e5c4c691488346f41c256e28bb45
Bruno Laureano <brunolsantos152@gmail.com>
2025-03-24 14:52:06 -0300

    Update ERC20WhitelistedAddresses.sol

--- ERC20/ERC20WhitelistedAddresses.sol
+++ ERC20/ERC20WhitelistedAddresses.sol
@@ -1,43 +1,65 @@
 // SPDX-License-Identifier: MIT
-// Creator: andreitoma8
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title WhitelistedERC20
+ * @dev ERC20 token that restricts token transfers to whitelisted addresses.
+ */
+contract WhitelistedERC20 is ERC20, Ownable {
 
+    /// @notice Mapping to track which addresses are whitelisted.
+    mapping(address => bool) public whitelist;
 
-contract ERC20WhitelistedAddresses is ERC20, Ownable {
-    
-    constructor(string memory _name, string memory _symbol)
-        ERC20(_name, _symbol)
-    {
+    /// @notice Emitted when an address's whitelist status is updated.
+    event WhitelistUpdated(address indexed account, bool status);
 
-        _mint(msg.sender, 1000000000000000000000000000000000 * 10 ** 18);
+    /**
+     * @dev Mints initial supply to the contract deployer.
+     * @param _name Name of the token.
+     * @param _symbol Symbol of the token.
+     * @param _initialSupply Initial token supply (in wei). 
+     *        Example: If you want 1,000 tokens and each token has 18 decimals,
+     *        pass 1000 * 10^18 = 1000000000000000000000.
+     */
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _initialSupply
+    ) ERC20(_name, _symbol) {
+        _mint(msg.sender, _initialSupply);
     }
 
-    //Mapping para determinar se o endereço em questão pertence a um grupo seleteo de endereços que podem manusear o token.
-    mapping(address => bool) public isAllowed;
-
-    function setIsAllowed(address _address, bool _bool) public onlyOwner {
-        isAllowed[_address] = _bool;
+    /**
+     * @dev Updates the whitelist status for a specific address.
+     * @param _account The address to update.
+     * @param _status True if the address is whitelisted, false otherwise.
+     */
+    function setWhitelistStatus(address _account, bool _status) external onlyOwner {
+        whitelist[_account] = _status;
+        emit WhitelistUpdated(_account, _status);
     }
 
+    /**
+     * @dev Overrides ERC20 transfer to require the recipient address be whitelisted.
+     * @param to The recipient of the tokens.
+     * @param amount The amount of tokens to transfer.
+     */
     function transfer(address to, uint256 amount) public override returns (bool) {
-        address owner = _msgSender();
-        _transfer(owner, to, amount);
-        return true;
+        require(whitelist[to], "Transfer to non-whitelisted address");
+        return super.transfer(to, amount);
     }
 
+    /**
+     * @dev Overrides ERC20 transferFrom to require the recipient address be whitelisted.
+     * @param from The address which is sending the tokens.
+     * @param to The address which is receiving the tokens.
+     * @param amount The amount of tokens to transfer.
+     */
     function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        require(isAllowed[to] == true, "Address 'to' not allowed to manage tokens");
-        address spender = _msgSender();
-        _spendAllowance(from, spender, amount);
-        _transfer(from, to, amount);
-        return true;
+        require(whitelist[to], "Transfer to non-whitelisted address");
+        return super.transferFrom(from, to, amount);
     }
-
-
 }
